5d8eb81c09fa0a7c91b08eccc1434186ec91a2c7
yansenzu <koderead00@gmail.com>
2021-02-24 20:45:59 +0700

    fix users controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\AdminAPI;
+
+class AdminController extends Controller
+{
+    public function loginadmin(Request $request){
+        $this->validate($request, [
+            'hondaid'  => 'required',
+            'password' => 'required',
+        ]);
+        $hondaid= $request->hondaid;
+        $password = $request->password;
+
+        $token = Str::random(60);
+        $api_token = hash('sha256', $token);
+        
+        $admin = AdminAPI::where([['hondaid', '=', $hondaid], ['password', '=', $password]])->first();
+        
+        if(!$admin)
+            return response()->json([
+                'status'  => 'error',
+                'message' => 'User not found'
+            ], 400);
+        $api = $admin->update([
+            'remember_token' => $api_token
+        ]);
+        $responseData = [
+            'status' => 'succes',
+            'user' => $admin,
+            'api_token' => $api_token
+        ];
+        return ($responseData);
+        
+    }
+    
+    public function adduser(Request $request){
+        $this->validate($request, [
+            'hondaid'     => 'required',
+            'role'  => 'required',
+            'namapic'    => 'required',
+            'password' => 'required',
+            'tempatlahir' => 'required',
+            'tgllahir' => 'required',
+            'jabatan' => 'required',
+            'status' => 'required',
+            'dealer' => 'required',
+        ]);
+            $hondaid = $request->hondaid;
+            $role = $request->role;
+            $namapic= $request->namapic;
+            $password = $request->password;
+            $tempatlahir = $request->tempatlahir;
+            $tgllahir = $request->tgllahir;
+            $jabatan = $request->jabatan;
+            $status = $request->status;
+            $dealer = $request->dealer;            
+
+        $Adminauth = AdminAPI::create([
+            'hondaid'     => $request->hondaid,
+            'role' => $request->role,
+            'namapic'    => $request->namapic,
+            'password' => $request->password,
+            'tempatlahir' => $request->tempatlahir,
+            'tgllahir' => $request->tgllahir,
+            'jabatan' => $request->jabatan,
+            'status' => $request->status,
+            'dealer' => $request->dealer,
+            'level' => 'basic',
+            'point' => 0
+
+        ]);
+            if($Adminauth){
+               return AdminAPI::where('hondaid', $hondaid)->first();
+            }
+            return(['error' => 'Gagal tambah akun !']);
+    }
+
+    public function edituser(request $request, $hondaid){
+        $namapic= $request->namapic;
+        $password = $request->password;
+        $tempatlahir = $request->tempatlahir;
+        $tgllahir = $request->tgllahir;
+        $jabatan = $request->jabatan;
+        $status = $request->status;
+        $dealer = $request->dealer;
+        $level = $request->level;
+        $point = $request->point;
+
+        $update = AdminAPI::find($hondaid);
+        $update->namapic = $namapic;
+        $update->password = $password;
+        $update->tempatlahir = $tempatlahir;
+        $update->jabatan = $jabatan;
+        $update->status = $status;
+        $update->dealer = $dealer;
+        $update->level = $level;
+        $update->point = $point;
+        $update->save();
+
+        if($update){
+        return AdminAPI::where('hondaid', $hondaid)->first();
+        }
+        return(['error' => 'Gagal update profile !']);
+    }
+
+    public function deleteuser($hondaid){
+        $role = AdminAPI::select('role')->where('hondaid', $hondaid)->get();
+        if($role != 'super admin'){
+            $user = AdminAPI::find($hondaid);
+            $user->delete();
+            return response()->json([
+                'status' => 'succes',
+                'message' => 'Berhasil Hapus Akun !'
+            ], 200);
+        }
+        else if($role == 'admin'){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Tidak bisa hapus'
+            ], 400);
+        }
+        else{
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Level anda bukan super admin'
+            ], 400);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UsersController;
-use App\Http\Controller\BukuTamuController;
-use App\Http\Controller\ProfileController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,12 +22,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/v1/users', [UsersController::class, 'index']);
-Route::get('/v1/users/{id}', [UsersController::class, 'usersbyid']);
-Route::post('/v1/users/register', [RegisterController::class, 'register']);
-Route::post('/v1/users/login', [LoginController::class, 'login']);
-Route::post('/v1/users/uploadbukutamu', [BukuTamuController::class, 'upload_bukutamu']);
-Route::post('/v1/users/deleteuser/{hondaid}', [UsersController::class, 'deleteuser']);
-Route::put('/v1/users/updateuser/{hondaid}', [UsersController::class, 'updateuser']);
 
-Route::get('/v1/users/profileusers', [UsersController::class, 'getprofile']);
+//Route Admin
+Route::post('/v1/admin/adduser', [AdminController::class, 'adduser']);
+Route::post('/v1/admin/edituser/{hondaid}', [AdminController::class, 'edituser']);
+Route::post('v1/admin/deleteuser/{hondaid}', [AdminController::class, 'deleteuser']);
+Route::post('/v1/admin/loginadmin', [AdminController::class, 'loginadmin']);
