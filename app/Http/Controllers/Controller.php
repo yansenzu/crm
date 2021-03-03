5e5fe44c7c45ce1b5b37595a57c5969949429729
Muhammad Irva <48151470+justirva09@users.noreply.github.com>
2021-03-03 15:40:16 +0700

    minor update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\File;
+use File;
 use Image;
 
 class Controller extends BaseController
